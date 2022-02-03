9ccd147f034a8a694289516921758eeddb0107ef
david-redick <david.a.redick@gmail.com>
2022-02-03 12:56:37 -0500

    creates a useful starting place for a new package

---
+++ scripts/print-package-template.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+printf '
+check_local () {
+	# A sanity check of the local system.
+	# Good place for things like CPU compatiblity, in case the application has inline assembler.
+	return 0
+}
+
+list_dependencies_debian () {
+	# Space delimited list of debian packages.
+	echo ""
+}
+
+list_dependencies_dirt () {
+	# Space delimited list of other dirt packages.
+	echo ""
+} 
+
+fetch () {
+	# Download the source code.
+	# Could be cloning the repo (preferred) or could be a packaged release bundle (tar ball, etc).
+	return 0
+}
+
+verify () {
+	# Perform any check sums or gpg signature verifications.
+	return 0
+}
+
+extract () {
+	# In the cases of bundled release (zip, etc), this step will unpack the bundle.
+	return 0
+}
+
+patch () {
+	# Known as prepare `prepare` in AUR.
+	return 0
+}
+
+configure () {
+	# Configure the source codes build setup.
+	return 0
+}
+
+build () {
+	# Compile and otherwise package up for installation or distribution.
+	return 0
+}
+
+test () {
+	# Run unit tests and perform compilation verification.
+	# Known as `check` in AUR.
+	return 0
+} 
+
+install () {
+	# Install the package to the local system.
+	return 0
+}
+
+check_install () {
+	# Any post install checks ands tests.
+	return 0
+}
+
+purge () {
+	# Remove any configuration (dot files) and other files created during run time.
+	return 0
+}
+'
