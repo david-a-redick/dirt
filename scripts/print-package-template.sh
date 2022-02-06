1c3772a3fedde947e5f2daaebd60247848ffeee8
david-redick <david.a.redick@gmail.com>
2022-02-06 15:21:53 -0500

    copyright

--- scripts/print-package-template.sh
+++ scripts/print-package-template.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Copyright 2020 - David A. Redick
+#
+# This file is part of dirt.
+#
+# dirt is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# dirt is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with dirt. If not, see <https://www.gnu.org/licenses/>. 
+
 printf '
 check_local () {
 	# A sanity check of the local system.
