a8c1e512e3543258ddcd4980eb6624200c2e9782
david-a-redick <81658937+david-a-redick@users.noreply.github.com>
2022-02-06 15:06:14 -0500

    Update dirt.sh

--- dirt.sh
+++ dirt.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# This file is part of dirt.
+#
+# dirt is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# dirt is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with dirt. If not, see <https://www.gnu.org/licenses/>. 
+
 # shows the commands being executed
 #set -x
 
